6220be5e878827392767c39f552754e3a71b1bbc
lalitkarki <lalitkarki1994@gmail.com>
2019-09-26 15:31:52 +0530

    fixed flake8 issues.

--- app/core/models.py
+++ app/core/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser, \
 
 # Create your models here.
 
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
@@ -13,9 +14,8 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
-
         return user
-    
+
     def create_super_user(self, email, password):
         user = self.create_user(email, password)
         user.is_staff = True
@@ -24,13 +24,13 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
+
     email = models.EmailField(max_length=255, unique=True)
-    name =  models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
-    
+
     def test_create_user_with_email_sucessful(self):
         """Test creating a new user with an email in successful"""
         email = 'test@londonappdev.com'
@@ -11,10 +12,9 @@ class ModelTests(TestCase):
             email=email,
             password=password
         )
-
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-    
+
     def test_new_user_email_normalized(self):
         """ Test the email for a new user is normalized"""
         email = 'test@LALITKARKI.COM'
